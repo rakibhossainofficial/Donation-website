a5eebe0e6813fbe23eedace317b3b5236d5464c8
rakibhossainofficial <rakib20@gmail.com>
2024-09-27 11:36:59 +0600

    Donate & History Btn

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,11 @@ module.exports = {
         "background": "#f9f7f3",
         "deep-black": "#111111",
         "dark-gray": "#585858",
-        "light-gray": "#A0A0A0",
-        "off-white": "#F3F3F3"
+        "light-gray": "#888888",
+        "medium-gray": "#A0A0A0",
+        "off-white": "#F3F3F3",
       },
+      
       fontFamily:{
         "lexend": "Lexend",
         
